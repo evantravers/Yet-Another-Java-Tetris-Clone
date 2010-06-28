9a19ddf3ea89bc45c0d940d383ad304623df1824
Evan Travers <widgetwebworks@gmail.com>
2010-06-28 13:27:09 -0500

    removed Test.java

--- Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-	public static void main(String args[]) {
-		System.out.println(new Piece(2,1));
-		System.out.println(new Piece(2,2));
-		System.out.println(new Piece(2,3));
-		System.out.println(new Piece(2,4));
-	}
-}
